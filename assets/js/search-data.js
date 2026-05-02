27142fe0494a4b7ce949e554d17c93fac6fe3e54
myamamoto26 <54725989+myamamoto26@users.noreply.github.com>
2026-05-02 16:10:35 +0000

    Deploying to gh-pages from @ myamamoto26/myamamoto26.github.io@de0e5803c31a5d09316d4c54943624a2c5bcba2b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -36,7 +36,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Updated April 2026. Click the right icon for the longer pdf version.",
           section: "Navigation menu",
           handler: () => {
             window.location.href = "/cv/";
